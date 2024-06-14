55f2bf7a1d494099610893358c832f713ff083bf
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-06-14 17:50:58 +0300

    bliblablu

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const thanos = async () => {};
+const thanos = async () => {
+  try {
+
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
+
+    
+    contacts = contacts.filter(() => Math.random() >= 0.5);
+
+    
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log(`Танос застосував свій щигель. Залишилось ${contacts.length} контактів.`);
+  } catch (error) {
+    console.error('Помилка:', error);
+  }
+};
 
 await thanos();
